639c2d678a97274829115d2507f487cbdde56b2b
battlesushi <105021030@live.asia.edu.tw>
2016-11-27 21:21:01 +0800

    hw
    This is fucking hard

--- src/hw/hw04.java
+++ src/hw/hw04.java
@@ -9,82 +9,121 @@ import java.util.Scanner;
 public class hw04 {
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		num x=new num();
-		x.led=sc.nextInt();
-		x.n=3;
-		run(x.ledFun());//跑 單一數字的位置
-		run(x.ledFun());
-		run(x.ledFun());
-		run(x.ledFun());
-		x.print();
-	}
-	public static void run(int z) {
-		num x=new num();
-		if(z==1){//2..3..4..5..6..7..8..9..0..
-			x.one();	x.num();// num為下個數字的位置移動
-		}
+		num numled=new num(sc.nextInt(),3);
+		numled.run();
 	}
 }
 class num{//陣列做格子    數字  空格   數字之間位置更動
-public short n;//n間隔  
-public int led,ans;//ans 取出單一數字
-private char data[][]=new char[9][23];
-	public int ledFun(){
-	ans=led%10;
-	led=led/10;
-	return ans;
-	}
-	public void num(){
-		n--;
-	}//陣列數字間隔n*(5+1)  陣列做的話(1 為數字之間空格部分 每個數字之間差6)
-	public void one(){
-		for(int i=0;i<5;i++){
-			data[i][4+n*(5+1)]='*';
-		}
-	}
-	public void two(){
-		
-	}
-	public void three(){
-		
-	}
-	public void four(){
-		
-	}
-	public void five(){
-		
-	}
-	public void six(){
-		
-	}
-	public void seven(){
-		
-	}
-	public void eight(){
-		
-	}
-	public void nine(){
-		
-	}
-	public void zero(){
-		
-	}
-	public void space(){
-		for(int i=0;i<5;i++){
-			for(int j=0;j<1;j++){
-				System.out.print("  ");
+public int n;//n間隔
+public int x;//輸入的數字
+public char data[][]=new char[9][23];
+	public num(int x1,int n1){
+		x=x1;	//輸入的數字
+		n=n1;	//n間隔
+	}					//陣列數字間隔n*(5+1)  陣列做的話(1 為數字之間空格部分 每個數字之間差6)
+	public void run() {
+		while(n>=0){
+			if(x%10==1){//2..3..4..5..6..7..8..9..0..
+				for(int i=0;i<9;i+=2){
+					data[i][4+n*(5+1)]='*';
+				}
 			}
-				System.out.println();
-				System.out.println();
+			else if(x%10==2){
+				for(int i=0;i<9;i+=4){//2的三橫排
+					for(int j=0;j<5;j++)
+					data[i][j+n*(5+1)]='*';
+				}
+					data[2][4+n*(5+1)]='*';//2的剩餘兩點
+					data[6][n*(5+1)]='*';	
+			}
+			else if(x%10==3){
+				for(int i=0;i<9;i+=4){//3的三橫排
+					for(int j=0;j<5;j++)
+					data[i][j+n*(5+1)]='*';
+				}
+					data[2][4+n*(5+1)]='*';//3的剩餘兩點
+					data[6][4+n*(5+1)]='*';
+			}
+			else if(x%10==4){
+				data[0][0+n*(5+1)]='*';
+				data[0][4+n*(5+1)]='*';
+				data[2][0+n*(5+1)]='*';
+				data[2][4+n*(5+1)]='*';
+				for(int i=0;i<5;i++){//4的橫排
+					data[4][i+n*(5+1)]='*';
+				}
+					data[6][4+n*(5+1)]='*';
+					data[8][4+n*(5+1)]='*';	
+			}
+			else if(x%10==5){
+				for(int i=0;i<9;i+=4){//5的三橫排
+					for(int j=0;j<5;j++)
+					data[i][j+n*(5+1)]='*';
+				}
+					data[2][n*(5+1)]='*';//5的剩餘兩點
+					data[6][4+n*(5+1)]='*';	
+			}
+			else if(x%10==6){
+				for(int i=0;i<9;i+=4){//6的三橫排
+					for(int j=0;j<5;j++)
+					data[i][j+n*(5+1)]='*';
+				}
+					data[2][n*(5+1)]='*';//剩餘三點
+					data[6][n*(5+1)]='*';
+					data[6][4+n*(5+1)]='*';	
+			}
+			else if(x%10==7){
+				for(int i=0;i<5;i++){//7的橫排
+					data[0][i+n*(5+1)]='*';
+				}
+				for(int i=2;i<9;i+=2){
+					data[i][4+n*(5+1)]='*';
+				}	
+			}
+			else if(x%10==8){
+					for(int i=0;i<9;i+=4){//8的三橫排
+						for(int j=0;j<5;j++)
+							data[i][j+n*(5+1)]='*';
+				}
+					data[2][0+n*(5+1)]='*';//8剩餘4點
+					data[2][4+n*(5+1)]='*';
+					data[6][0+n*(5+1)]='*';
+					data[6][4+n*(5+1)]='*';	
+			}
+			else if(x%10==9){
+					for(int i=0;i<5;i+=4){//9的兩橫排
+						for(int j=0;j<5;j++)
+							data[i][j+n*(5+1)]='*';
+					}
+					data[2][0+n*(5+1)]='*';//9剩餘4點
+					data[2][4+n*(5+1)]='*';
+					data[6][4+n*(5+1)]='*';
+					data[6][4+n*(5+1)]='*';	
+			}
+			else if(x%10==0){
+					for(int i=0;i<9;i+=8){//0的兩橫排
+						for(int j=0;j<5;j++)
+							data[i][j+n*(5+1)]='*';
+					}
+					data[2][0+n*(5+1)]='*';//0剩餘6點
+					data[2][4+n*(5+1)]='*';
+					data[4][0+n*(5+1)]='*';
+					data[4][4+n*(5+1)]='*';
+					data[6][0+n*(5+1)]='*';
+					data[6][4+n*(5+1)]='*';	
+			}
+			x=x/10;
+			n--;
 		}
-	}
-	public void print(){
+		
+		
 		for(int i=0;i<9;i++){
 			for(int j=0;j<23;j++){
 				System.out.print(data[i][j]);
 			}
-			System.out.println();
-		}
-	}		
+				System.out.println();
+			}
+		
+	}//run		
 	
 }
