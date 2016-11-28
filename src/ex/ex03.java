3d48096bafc92f1c63abd92dca3532513d414808
battlesushi <105021030@live.asia.edu.tw>
2016-11-28 14:27:36 +0800

    ex

--- src/ex/ex03.java
+++ src/ex/ex03.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 /*
  * Topic:  寫一程式計算兩個矩陣的乘法，例如 A 是 n1 x m1 的矩陣， B 是 n2 x m2 的矩 陣，C=AB 為 n1 x m2 矩陣
-	第一列輸入 n1, m1, n2 及 m2 等 4 個整數，n1, m1 是第一個陣列的列(row)與行 (column)，而 n2, m2 則是第二個陣列的列(row)與行(column)，其中 m2 = n2。接著輸 入兩個陣列的內容。
+	第一列輸入 n1, m1, n2 及 m2 等 4 個整數，n1, m1 是第一個陣列的列(row)與行 (column)，而 n2, m2 則是第二個陣列的列(row)與行(column)，其中 m1 = n2。接著輸 入兩個陣列的內容。
  * Date: 2016/11/21
  * Author: 105021030 張廷毓
  */
@@ -30,7 +30,7 @@ public class ex03 {
 		for(int i=0;i<A.length;i++){
 			for(int j=0;j<B[0].length;j++){
 				sum=0;
-				for(int k=0;k<A[0].length;k++){ //因為 n2=m2  加的次數也隨之而定
+				for(int k=0;k<A[0].length;k++){ //因為 m1=n2  加的次數也隨之而定
 					sum=sum+A[i][k]*B[k][j];
 				}
 				C[i][j]=sum;
